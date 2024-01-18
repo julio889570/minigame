95e3965127fc7b5f48b1d8dbe69bdd83d6c1be2e
julio889570 <julio889570@gmail.com>
2024-01-18 16:41:23 -0400

    second

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
 
+import { StyleSheet, ImageBackground} from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
   return (
-    <StartGameScreen />
+    <LinearGradient colors={['#ffb9b9','#a31818','#990012','#ee7272']} style={styles.rootScreen}>
+       <ImageBackground source={require('./assets/images/adaptive-icon.png')}
+       resizeMode="cover"
+       style={styles.rootScreen}
+       imageStyle={styles.backgroundImage}
+       
+       >
+        <StartGameScreen />
+        </ImageBackground>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen:{
+    flex: 1,
+  },
+  backgroundImage:{
+    opacity: 0.15
+  }
+});
