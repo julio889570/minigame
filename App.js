9c42179bf0abcb986a62da88046b2c3724669cfa
julio889570 <julio889570@gmail.com>
2024-01-22 20:52:46 -0400

    first

--- App.js
+++ App.js
@@ -2,7 +2,18 @@
 import { StyleSheet, ImageBackground} from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from 'react';
+import GameScreen from './screens/GameScreen';
 export default function App() {
+  const [userNumber, setUserNumber] = useState(0)
+  function pickedNumberHandler(pickedNumber){
+    setUserNumber(pickedNumber)
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>
+ if (userNumber){
+    screen = <GameScreen />
+ }
   return (
     <LinearGradient colors={['#ffb9b9','#a31818','#990012','#ee7272']} style={styles.rootScreen}>
        <ImageBackground source={require('./assets/images/adaptive-icon.png')}
@@ -11,7 +22,7 @@ export default function App() {
        imageStyle={styles.backgroundImage}
        
        >
-        <StartGameScreen />
+        {screen}
         </ImageBackground>
     </LinearGradient>
   );
